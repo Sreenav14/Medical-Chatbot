557fd23ff104524a40ef7f18864b9721342eba77
Sreenav14 <sreenavjajerao@gmail.com>
2025-09-23 00:43:59 -0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.1.0",
+    author="Sreenav Jajerao",
+    author_email="sreenavjajerao@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
